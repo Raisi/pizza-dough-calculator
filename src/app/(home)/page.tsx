9a1f0c7ad5e0d3a5c601e089230a943bccda6e42
Rainer Angerer <r.angerer@durchblicker.at>
2023-10-23 21:56:40 +0200

    add 3d Pizza

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,38 @@
+'use client';
 import FullPoolishCalculator from '@/components/FullPoolishCalculator';
-/* import { useState, Suspense } from 'react';
+import { useState, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 
 function PizzaModel() {
 	const gltf = useGLTF('/models/pizza.glb');
-	return <primitive object={gltf.scene} />;
+	return <primitive object={gltf.scene} tra />;
 }
- */
+
 export default function Home() {
 	return (
 		<main className='grid w-[100vw] h-[100vh] place-items-center'>
 			<section className='container mx-auto max-w-4xl text-center'>
-				{/* <Canvas style={{ position: 'absolute', zIndex: -1, top: 0, left: 0 }}>
+				<Canvas
+					style={{
+						position: 'absolute',
+						zIndex: -1,
+						top: '10vh',
+						left: 0,
+						width: '50vw',
+						height: '90vh'
+					}}
+					camera={{
+						type: 'perspective',
+						zoom: 2, // Control the zoom level
+						position: [10, 4, 10] // Set the initial position
+					}}
+				>
 					<ambientLight />
 					<Suspense fallback={null}>
 						<PizzaModel />
 					</Suspense>
-				</Canvas> */}
+				</Canvas>
 				<FullPoolishCalculator />
 			</section>
 		</main>
